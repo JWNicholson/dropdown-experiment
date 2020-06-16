4b4af7a249b98e9780a85b7354c65decb65df871
JW Nicholson <john.nicholson812@gmail.com>
2020-06-16 13:55:41 -0400

    setup Dropdown menu

--- src/App.js
+++ src/App.js
@@ -5,20 +5,16 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <div className="container">
+       <button type="button" className="toggle">X</button>
+       <div className="dropdown">
+         <ul>
+           <li>AAAA</li>
+           <li>BBBB</li>
+           <li>CCCC</li>
+         </ul>
+       </div>
+     </div>
     </div>
   );
 }
